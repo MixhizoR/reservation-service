ee4a3cba7cff6d2ca381ce814d89b1f75f8e7599
MixhizoR <oguzselmancetin@hotmail.com>
2026-01-29 18:27:59 +0300

    Initial Commit

---
+++ src/main/java/com/omniticket/reservation_service/OmniTicketApplication.java
@@ -0,0 +1,13 @@
+package com.omniticket.reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omniticket/reservation_service/OmniTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omniticket.reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
